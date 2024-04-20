d9646693eb1659395f39d74856a24fa414312568
TatianaSkl <sklyarchyk@gmail.com>
2024-04-20 22:43:15 +0300

    add images+icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import type { Metadata } from 'next';
+import { ToastContainer } from 'react-toastify';
+import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <ToastContainer autoClose={2500} />
+      </body>
     </html>
   );
 }
