bd5caf51f86ce8ee118035a2283c2a13a43534d7
TatianaSkl <sklyarchyk@gmail.com>
2024-04-26 23:12:49 +0300

    add gallery section

---
+++ src/components/modules/SliderGallery.tsx
@@ -0,0 +1,63 @@
+import { FC } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import Image from 'next/image';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import gallery from '@/data/gallery.json';
+
+const SliderGallery: FC = () => {
+  return (
+    <div className="relative">
+      <Swiper
+        className="h-[609px] m:h-[306px] l:h-[437px]"
+        modules={[Navigation]}
+        wrapperTag="ul"
+        loop={true}
+        direction="vertical"
+        centeredSlides={true}
+        grabCursor={true}
+        slidesPerView={3}
+        spaceBetween={24}
+        navigation={{
+          nextEl: '.btn-next',
+          prevEl: '.btn-prev',
+        }}
+        breakpoints={{
+          768: {
+            direction: 'horizontal',
+          },
+        }}
+      >
+        {gallery.slider.map(({ id, src, alt }) => (
+          <SwiperSlide key={id} tag="li" className="gallery-slider">
+            <Image
+              src={src}
+              alt={alt}
+              sizes="(min-width: 1280px) 606px, (min-width: 768px) 415px, 440px"
+              fill
+              className="object-cover"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <button
+        type="button"
+        aria-label="button back"
+        className="btn-prev md:hidden absolute z-10 left-[37px] bottom-[17px] text-[33px] font-thin leading-[1.2] l:bottom-0 l:left-[228px]"
+      >
+        {gallery.btnBack}
+      </button>
+      <button
+        type="button"
+        aria-label="button next"
+        className="btn-next md:hidden absolute z-10 right-[37px] bottom-[17px] text-[33px] font-thin leading-[1.2] l:bottom-0 l:right-[228px]"
+      >
+        {gallery.btnNext}
+      </button>
+    </div>
+  );
+};
+
+export default SliderGallery;
--- src/sections/Gallery.tsx
+++ src/sections/Gallery.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import { FC } from 'react';
+import { TitleSection } from '@/components/ui-kit';
+import gallery from '@/data/gallery.json';
+import SliderGallery from '@/components/modules/SliderGallery';
 
 const Gallery: FC = () => {
   return (
     <section className="section gallery-bg-m m:gallery-bg-t l:gallery-bg-d" id="gallery">
-      <div className="container"></div>
+      <div className="container l:px-0">
+        <div className="mb-6 m:mx-auto m:w-[464px] m:mb-[72px] l:w-[657px] l:mr-auto l:ml-0 l:mb-6 l:pl-6">
+          <TitleSection text={gallery.title} textAccent={gallery.titleAccent} />
+        </div>
+        <SliderGallery />
+      </div>
     </section>
   );
 };
