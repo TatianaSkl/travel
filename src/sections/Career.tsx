de435d1214ee59a5a4c5b3a5b354d7a9cba158e8
TatianaSkl <sklyarchyk@gmail.com>
2024-04-27 09:43:39 +0300

    updated career section

--- src/sections/Career.tsx
+++ src/sections/Career.tsx
@@ -7,10 +7,31 @@ import { FormCareer } from '@/components/modules';
 
 const Career: FC = () => {
   return (
-    <>
-      <section className="section career-bg-m m:career-bg-t l:career-bg-d" id="career">
+    <section id="career">
+      <div className="section career-bg-m m:hidden">
         <div className="container">
-          <div className="m:flex m:justify-between">
+          <div className="mb-6">
+            <TitleSection text={career.title} textAccent={career.titleAccent} />
+          </div>
+          <p className="w-[179px] ml-auto mb-9 font-extralight leading-[20px]">
+            {career.description}
+          </p>
+          <p className="uppercase w-[181px] ml-auto text-[30px] font-extralight leading-[1.2] mb-9 pr-10">
+            {career.question}
+          </p>
+          <ul className="w-[181px] text-right flex flex-col gap-4">
+            {career.info.map(({ id, title, text }) => (
+              <li key={id}>
+                <p className="leading-[20px] mb-2">{title}</p>
+                <p className="text-[12px] font-extralight leading-[20px]">{text}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="section career-bg-m m:career-bg-t l:career-bg-d">
+        <div className="container">
+          <div className="md:hidden m:flex m:justify-between">
             <div className="mb-6 m:mb-3 l:mb-6">
               <TitleSection text={career.title} textAccent={career.titleAccent} />
             </div>
@@ -18,7 +39,7 @@ const Career: FC = () => {
               {career.description}
             </p>
           </div>
-          <div className="l:flex l:gap-[333px]">
+          <div className="md:hidden l:flex l:gap-[333px]">
             <p className="uppercase w-[181px] ml-auto text-[30px] font-extralight leading-[1.2] mb-9 pr-10 m:w-[221px] m:ml-0 m:pr-0 m:pl-20 m:mb-14 l:w-[293px] l:pl-[123px] l:text-[36px] l:leading-[39px] l:mb-[45px]">
               {career.question}
             </p>
@@ -28,7 +49,7 @@ const Career: FC = () => {
             </div>
           </div>
           <div className="m:flex m:gap-5 l:gap-6">
-            <ul className="w-[181px] text-right flex flex-col gap-4 m:w-[221px] m:gap-6 l:w-[602px]">
+            <ul className="md:hidden w-[181px] text-right flex flex-col gap-4 m:w-[221px] m:gap-6 l:w-[602px]">
               {career.info.map(({ id, title, text }) => (
                 <li key={id} className="l:flex l:gap-6">
                   <p className="leading-[20px] mb-2 l:leading-[24px] l:mb-0 l:w-[293px]">{title}</p>
@@ -38,8 +59,8 @@ const Career: FC = () => {
                 </li>
               ))}
             </ul>
-            <div className="md:hidden">
-              <div className="m:w-[170px] m:text-[13px] m:font-extralight m:leading-[20px] m:mb-8 l:hidden">
+            <div>
+              <div className="w-[179px] mb-6 ml-auto font-extralight leading-[20px] m:w-[170px] m:ml-0 m:text-[13px] m:font-extralight m:leading-[20px] m:mb-8 l:hidden">
                 <p>{career.text[0]}</p>
                 <p>{career.text[1]}</p>
               </div>
@@ -47,17 +68,8 @@ const Career: FC = () => {
             </div>
           </div>
         </div>
-      </section>
-      <section className="section career-bg-m m:hidden">
-        <div className="container">
-          <div className="w-[179px] mb-6 ml-auto font-extralight leading-[20px]">
-            <p>{career.text[0]}</p>
-            <p>{career.text[1]}</p>
-          </div>
-          <FormCareer />
-        </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
